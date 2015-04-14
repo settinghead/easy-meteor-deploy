d025dbfe021bd0dea7433dcf7b3d38013e3b629f
Xiyang Chen <settinghead@gmail.com>
2015-04-13 20:19:35 -0400

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,11 +6,10 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 ansible-playbook -i $DIR/server-inventories/$ENV $DIR/playbook-setup.yml
 echo "Deploying app..."
-cd ../app
+cd ../
 echo $DIR/server-inventories/_vagrant
 ansible-playbook -i $DIR/server-inventories/$ENV $DIR/playbook-build-app.yml --connection=local
-cd ../deploy
 ansible-playbook -i $DIR/server-inventories/$ENV -M ./library $DIR/playbook-app-only.yml
 
 # wait
-echo 'Deploy finished running on vagrant.'
+echo Deployment finished for $ENV.

3c59eada94828bd9069f774cc84b1802f6364f88
Xiyang Chen <settinghead@gmail.com>
2015-04-29 22:25:27 -0400

    Make deploy.sh more verbose

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 set -e
+set -x
+
 : ${ENV?"Need to set ENV"}
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-ansible-playbook -i $DIR/server-inventories/$ENV $DIR/playbook-setup.yml
+ansible-playbook -i $DIR/server-inventories/$ENV $DIR/playbook-setup.yml -v
 echo "Deploying app..."
 cd ../
-echo $DIR/server-inventories/_vagrant
-ansible-playbook -i $DIR/server-inventories/$ENV $DIR/playbook-build-app.yml --connection=local
-ansible-playbook -i $DIR/server-inventories/$ENV -M ./library $DIR/playbook-app-only.yml
+echo $DIR/server-inventories/$ENV
+ansible-playbook -i $DIR/server-inventories/$ENV $DIR/playbook-build-app.yml --connection=local -v
+ansible-playbook -i $DIR/server-inventories/$ENV -M ./library $DIR/playbook-app-only.yml -v
 
 # wait
 echo Deployment finished for $ENV.
